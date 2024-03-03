6fc72da217b7dc9dbc21dac48c72fee8af621f9e
RdotSilva <RyandotSilva@gmail.com>
2024-03-03 17:38:21 +0000

    Add mock item loc data

---
+++ src/data/mockItems.ts
@@ -0,0 +1,32 @@
+import { LocItem } from "../types/LocItem";
+
+export const mockItemList: LocItem[] = [
+  {
+    id: 1,
+    zone: "Sebilis",
+    command: "#zone sebilis 209 -1623 -122",
+    description: "Main entrance",
+    category: "hunt",
+  },
+  {
+    id: 2,
+    zone: "Sebilis",
+    command: "#zone sebilis 209 -1623 -122",
+    description: "South area",
+    category: "hunt",
+  },
+  {
+    id: 3,
+    zone: "Sebilis",
+    command: "#zone sebilis -758 -1917 -130",
+    description: "Trakanon boss",
+    category: "plat",
+  },
+  {
+    id: 4,
+    zone: "Guild Lobby",
+    command: "#zone guildlobby -74 424 10",
+    description: "Merchant Drake",
+    category: "npc",
+  },
+];
