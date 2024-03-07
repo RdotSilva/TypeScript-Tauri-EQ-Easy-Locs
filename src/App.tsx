9fb8b73d81104ac1515ea7033f0cfaff614b845c
RdotSilva <RyandotSilva@gmail.com>
2024-03-07 17:37:54 +0000

    Add fetchParsedCsvData logic to main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect, useState } from "react";
 import "./App.css";
-import ItemList from "./components/ItemList";
-import { mockItemList } from "./data/mockItems";
+import ItemList from "./components/ItemList/ItemList";
+import { appWindow } from "@tauri-apps/api/window";
+import { parseCSVFile } from "./utils/csvParser";
+
+import "../src/components/ItemList/ItemList.css";
+import { LocItem } from "./types/LocItem";
+
+await appWindow.setAlwaysOnTop(true);
 
 function App() {
-  const [items, setItems] = useState(mockItemList);
+  const [items, setItems] = useState<LocItem[]>([]);
+
+  useEffect(() => {
+    const fetchParsedCsvData = async () => {
+      try {
+        const response = await parseCSVFile();
+        const locItems = response.data as LocItem[];
+        setItems(locItems);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setItems([]);
+      }
+    };
+
+    fetchParsedCsvData();
+  }, []);
 
   return (
     <div className="container">
-      <h1>Welcome to EQ Easy Locs</h1>
-
       <div className="row">
-        <ItemList items={items} />
+        {items === null ? <p>Loading...</p> : <ItemList items={items} />}
       </div>
     </div>
   );
