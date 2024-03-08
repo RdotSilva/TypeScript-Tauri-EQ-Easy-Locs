d0b7c48d57e436ea7d2b0e219f798b64c7f05e13
RdotSilva <RyandotSilva@gmail.com>
2024-03-08 19:47:40 +0000

    Refactor parseCSVFile function

--- src/utils/csvParser.ts
+++ src/utils/csvParser.ts
@@ -2,13 +2,12 @@ import { readTextFile } from "@tauri-apps/api/fs";
 import Papa from "papaparse";
 
 import { desktopDir } from "@tauri-apps/api/path";
-import { LocItem } from "../types/LocItem";
 
 /**
  * Read loc information from a file and parse into JSON
  * @returns - A list of loc items
  */
-export const parseCSVFile: LocItem[] = async () => {
+export const parseCSVFile = async () => {
   const desktopPath = await desktopDir();
 
   const locDataFile = `${desktopPath}/locs.csv`;
@@ -17,6 +16,5 @@ export const parseCSVFile: LocItem[] = async () => {
 
   const parsedItemResponse = Papa.parse(fileContents, { header: true });
 
-  console.log(`Returning Items: ${JSON.stringify(parsedItemResponse.data)}`);
-  return parsedItemResponse.data as LocItem[];
+  return parsedItemResponse;
 };
