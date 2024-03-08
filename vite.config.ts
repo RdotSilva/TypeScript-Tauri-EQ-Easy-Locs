a2c7754cfa35d61da38d0304e3dff3260004fdb4
RdotSilva <RyandotSilva@gmail.com>
2024-03-08 19:46:58 +0000

    Update build target to fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+  build: {
+    target: "esnext", // browsers can handle the latest ES features
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
