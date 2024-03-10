32a713e52f6897e20474861c00129e91417afa18
RdotSilva <RyandotSilva@gmail.com>
2024-03-10 14:08:21 +0000

    Add FilterSelectInput component

---
+++ src/components/FilterSelectInput/FilterSelectInput.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface FilterSelectsProps {
+  uniqueOptions: string[];
+  selectedValue: string | null;
+  label: string;
+  onSelectChange: (value: string) => void;
+}
+
+const FilterSelectInput: React.FC<FilterSelectsProps> = ({
+  uniqueOptions,
+  selectedValue,
+  label,
+  onSelectChange,
+}) => {
+  return (
+    <div className="select-group">
+      <label htmlFor={`${label}Selector`}>{label}:</label>
+      <select
+        id={`${label}Selector`}
+        onChange={(e) => onSelectChange(e.target.value)}
+        value={selectedValue || ""}
+      >
+        <option value="">All {label}s</option>
+        {uniqueOptions.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default FilterSelectInput;
