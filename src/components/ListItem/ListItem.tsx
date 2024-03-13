6fb2f78c35e0e3548a24495e523af0bb001015d2
RdotSilva <RyandotSilva@gmail.com>
2024-03-13 21:07:46 +0000

    Add ListItem component

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { LocItem } from "../../types/LocItem";
 
-const ListItem = () => {
-  return <>ListItem</>;
+const ListItem: React.FC<{ item: LocItem }> = ({ item }) => {
+  return (
+    <li className="item">
+      <div>
+        <strong>Zone:</strong> {item.zone}
+      </div>
+      <div>
+        <strong>Command:</strong> {item.command}
+      </div>
+      <div>
+        <strong>Description:</strong> {item.description}
+      </div>
+      {item.category && (
+        <div>
+          <strong>Category:</strong> {item.category}
+        </div>
+      )}
+    </li>
+  );
 };
 
 export default ListItem;
