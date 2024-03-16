af501ee87a29386ecb34840d67000291af501037
RdotSilva <RyandotSilva@gmail.com>
2024-03-16 18:41:58 +0000

    Add dividers

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { LocItem } from "../../types/LocItem";
 import SendCommandButton from "../SendCommandButton/SendCommandButton";
 import CopyToClipboardButton from "../CopyToClipboardButton/CopyToClipboardButton";
+import "./ListItem.css";
 
 const ListItem: React.FC<{ item: LocItem; key: string | number }> = ({
   item,
@@ -9,10 +10,14 @@ const ListItem: React.FC<{ item: LocItem; key: string | number }> = ({
 }) => {
   return (
     <li key={key} className="item">
-      <div>{item.zone}</div>
-      <div>{item.command}</div>
-      <div>{item.description}</div>
-      {item.category && <div>{item.category}</div>}
+      <div className="item-zone">{item.zone}</div>
+      <hr className="divider" />
+      <div className="item-command">{item.command}</div>
+      <hr className="divider" />
+      <div className="item-description">{item.description}</div>
+      <hr className="divider" />
+      {item.category && <div className="item-category">{item.category}</div>}
+      <hr className="divider" />
       <CopyToClipboardButton text={item.command} />
       <SendCommandButton command={item.command} />
     </li>
