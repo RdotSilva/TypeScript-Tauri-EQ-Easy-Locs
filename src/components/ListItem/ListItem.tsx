05c8ab6b6d81f584fc6fdf912046618f86d0d5e5
RdotSilva <RyandotSilva@gmail.com>
2024-03-17 21:01:34 +0000

    Refactor dividers

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -3,6 +3,7 @@ import { LocItem } from "../../types/LocItem";
 import SendCommandButton from "../SendCommandButton/SendCommandButton";
 import CopyToClipboardButton from "../CopyToClipboardButton/CopyToClipboardButton";
 import "./ListItem.css";
+import { Divider } from "@mui/material";
 
 const ListItem: React.FC<{ item: LocItem; key: string | number }> = ({
   item,
@@ -11,13 +12,13 @@ const ListItem: React.FC<{ item: LocItem; key: string | number }> = ({
   return (
     <li key={key} className="item">
       <div className="item-zone">{item.zone}</div>
-      <hr className="divider" />
+      <Divider />
       <div className="item-command">{item.command}</div>
-      <hr className="divider" />
+      <Divider />
       <div className="item-description">{item.description}</div>
-      <hr className="divider" />
+      <Divider />
       {item.category && <div className="item-category">{item.category}</div>}
-      <hr className="divider" />
+      <Divider />
       <CopyToClipboardButton text={item.command} />
       <SendCommandButton command={item.command} />
     </li>
