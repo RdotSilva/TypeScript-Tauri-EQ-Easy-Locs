e6eae51cd4e5a09314b016d1fbaad120ac45a343
RdotSilva <RyandotSilva@gmail.com>
2024-03-24 16:26:52 +0000

    Add StyledButton component

--- src/components/CopyToClipboardButton/CopyToClipboardButton.tsx
+++ src/components/CopyToClipboardButton/CopyToClipboardButton.tsx
@@ -2,12 +2,23 @@ import React from "react";
 
 import { Button, styled } from "@mui/material";
 
+const StyledButton = styled(Button)`
+  margin-top: 5px;
+  padding: 5px 10px;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  background-color: orange;
+`;
+
 const CopyToClipboardButton: React.FC<{ text: string }> = ({ text }) => {
   const copyToClipboard = () => {
     navigator.clipboard.writeText(text);
   };
 
-  return <Button onClick={copyToClipboard}>Copy to Clipboard</Button>;
+  return (
+    <StyledButton onClick={copyToClipboard}>Copy to Clipboard</StyledButton>
+  );
 };
 
 export default CopyToClipboardButton;
