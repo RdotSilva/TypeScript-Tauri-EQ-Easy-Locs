557a2341ec62721f1323331cb48aebed5ddf4a41
RdotSilva <RyandotSilva@gmail.com>
2024-03-30 21:16:08 +0000

    Add FormControl for select

--- src/components/FilterSelectInput/FilterSelectInput.tsx
+++ src/components/FilterSelectInput/FilterSelectInput.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { pluralizeLabel } from "../../utils/pluralizeLabel";
-import { Select } from "@mui/material";
+import { FormControl, Select } from "@mui/material";
 
 interface FilterSelectsProps {
   uniqueOptions: string[];
@@ -20,19 +20,20 @@ const FilterSelectInput: React.FC<FilterSelectsProps> = ({
 }) => {
   return (
     <div className="select-group">
-      <label htmlFor={`${label}Selector`}>{label}:</label>
-      <Select
-        id={`${label}Selector`}
-        onChange={(e) => onSelectChange(e.target.value)}
-        value={selectedValue || ""}
-      >
-        <option value="">All {pluralizeLabel(label)}</option>
-        {uniqueOptions.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-      </Select>
+      <FormControl fullWidth>
+        <Select
+          id={`${label}Selector`}
+          onChange={(e) => onSelectChange(e.target.value)}
+          value={selectedValue || ""}
+        >
+          <option value="">All {pluralizeLabel(label)}</option>
+          {uniqueOptions.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </Select>
+      </FormControl>
     </div>
   );
 };
