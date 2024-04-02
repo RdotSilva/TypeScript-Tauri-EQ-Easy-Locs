0a4d0dd6652b68b334c0b0965bad7ff99e550a03
RdotSilva <RyandotSilva@gmail.com>
2024-04-02 17:54:29 +0100

    Add send_command_to_window command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -18,10 +18,50 @@ fn send_input(text: String) {
     println!("Received text: {}", text);
 }
 
+// This works but ONLY for Notepad
+// Doesn't work for EQ
+#[tauri::command]
+fn send_command_to_window(command: String) {
+    let window_class_name = CString::new("NOTEPAD").expect("Failed to create CString");
+    let window_handle = unsafe { FindWindowA(window_class_name.as_ptr(), ptr::null()) };
+
+    if window_handle != ptr::null_mut() {
+        // Focus on the EverQuest window
+        unsafe {
+            SetForegroundWindow(window_handle);
+        }
+
+        // Unsafe block for calling SendInput
+        unsafe {
+            // Send each character of the command to the window using SendInput
+            for c in command.chars() {
+                let mut input = INPUT {
+                    type_: INPUT_KEYBOARD,
+                    u: std::mem::zeroed(),
+                };
+
+                input.u.ki_mut().wVk = 0;
+                input.u.ki_mut().wScan = c as u16;
+                input.u.ki_mut().dwFlags = KEYEVENTF_UNICODE;
+
+                // Press
+                SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
+
+                // Release
+                input.u.ki_mut().dwFlags |= KEYEVENTF_KEYUP;
+                SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
+            }
+        }
+    } else {
+        println!("EverQuest window not found");
+    }
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![send_input])
+        .invoke_handler(tauri::generate_handler![send_command_to_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
