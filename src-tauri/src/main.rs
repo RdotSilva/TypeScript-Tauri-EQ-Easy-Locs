10ef4061c88ae3c223fcb693b0ad8d2fd06c7e1c
RdotSilva <RyandotSilva@gmail.com>
2024-04-02 17:54:06 +0100

    Add send_input command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::ffi::CString;
+use std::ptr;
+
+use winapi::um::winuser::{FindWindowA, SetForegroundWindow};
+use winapi::um::winuser::{SendInput, INPUT, INPUT_KEYBOARD, KEYEVENTF_KEYUP, KEYEVENTF_UNICODE};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn send_input(text: String) {
+    println!("Received text: {}", text);
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![send_input])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
