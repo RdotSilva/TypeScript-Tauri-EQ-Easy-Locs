3c9f567d6c1d8af7760b5d0b80dbddf2b6ace0cb
RdotSilva <RyandotSilva@gmail.com>
2024-04-04 19:00:57 +0100

    Refactor using new hook

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { LocItem } from "../../types/LocItem";
 import FilterSelectInput from "../FilterSelectInput/FilterSelectInput";
 import LocDetails from "../LocDetails/LocDetails";
 import { Box, Container, styled } from "@mui/material";
+import useFilterItems from "../../hooks/useFilterItems";
 
 export const StyledSelectContainer = styled(Box)`
   display: flex;
@@ -13,29 +14,19 @@ interface ListProps {
 }
 
 const ItemList: React.FC<ListProps> = ({ items }) => {
-  const [selectedZone, setSelectedZone] = useState<string | null>(null);
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-
-  const filteredItems = items.filter((item) => {
-    const zoneCondition = !selectedZone || item.zone === selectedZone;
-    const categoryCondition =
-      !selectedCategory || item.category === selectedCategory;
-    return zoneCondition && categoryCondition;
-  });
-
-  const uniqueZones = [...new Set(items.map((item) => item.zone))];
+  const {
+    filteredItems,
+    selectedZone,
+    selectedCategory,
+    handleZoneChange,
+    handleCategoryChange,
+  } = useFilterItems(items);
 
   const uniqueCategories = [...new Set(items.map((item) => item.category))]
     .filter((category) => category !== undefined)
     .map((category) => category as string);
 
-  const handleZoneChange = (value: string) => {
-    setSelectedZone(value);
-  };
-
-  const handleCategoryChange = (value: string) => {
-    setSelectedCategory(value);
-  };
+  const uniqueZones = [...new Set(items.map((item) => item.zone))];
 
   return (
     <Container>
