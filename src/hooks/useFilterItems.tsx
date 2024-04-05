8d1d8db650e0a0bd449df83c5d0eb9975896bdca
RdotSilva <RyandotSilva@gmail.com>
2024-04-05 21:08:35 +0100

    Add unique sets to hook

--- src/hooks/useFilterItems.tsx
+++ src/hooks/useFilterItems.tsx
@@ -5,6 +5,12 @@ const useFilterItems = (items: LocItem[]) => {
   const [selectedZone, setSelectedZone] = useState<string | null>(null);
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
+  const uniqueCategories = [...new Set(items.map((item) => item.category))]
+    .filter((category) => category !== undefined)
+    .map((category) => category as string);
+
+  const uniqueZones = [...new Set(items.map((item) => item.zone))];
+
   const filterItems = (items: LocItem[]) => {
     return items.filter((item) => {
       const zoneCondition = !selectedZone || item.zone === selectedZone;
@@ -28,6 +34,8 @@ const useFilterItems = (items: LocItem[]) => {
     selectedCategory,
     handleZoneChange,
     handleCategoryChange,
+    uniqueCategories,
+    uniqueZones,
   };
 };
 
