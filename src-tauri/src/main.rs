83ad39be48dc7e5980cdfed05a92915864c77064
RdotSilva <RyandotSilva@gmail.com>
2024-04-06 10:30:34 +0100

    Add change_window_title function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,19 @@
 
 use std::ffi::CString;
 use std::ptr;
+use winapi::um::winuser::{
+    FindWindowA, SendMessageA, SetForegroundWindow, BM_CLICK, WM_CHAR, WM_SETTEXT,
+};
+use winapi::um::winuser::{
+    SendInput, INPUT, INPUT_KEYBOARD, KEYBDINPUT, KEYEVENTF_KEYUP, KEYEVENTF_UNICODE,
+};
 
 use winapi::um::winuser::{FindWindowA, SetForegroundWindow};
-use winapi::um::winuser::{SendInput, INPUT, INPUT_KEYBOARD, KEYEVENTF_KEYUP, KEYEVENTF_UNICODE};
+use winapi::um::winuser::{
+    SendInput, INPUT, INPUT_KEYBOARD, KEYBDINPUT, KEYEVENTF_KEYUP, KEYEVENTF_UNICODE,
+};
+
+use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -16,6 +26,35 @@ fn greet(name: &str) -> String {
 #[tauri::command]
 fn send_input(text: String) {
     println!("Received text: {}", text);
+}
+
+// This works but it changes the WINDOW TITLE (doesn't send text to the window)
+#[tauri::command]
+fn change_window_title(command: String) {
+    let window_class_name = CString::new("Notepad").expect("Failed to create CString");
+    let window_handle = unsafe { FindWindowA(window_class_name.as_ptr(), ptr::null()) };
+
+    if window_handle != ptr::null_mut() {
+        let command_cstring = CString::new(command).expect("Failed to create CString");
+
+        // Set the text of the window (if applicable)
+        unsafe {
+            SendMessageA(
+                window_handle,
+                WM_SETTEXT,
+                0,
+                command_cstring.as_ptr() as isize,
+            );
+        }
+
+        // Optionally, you can simulate a button click if needed
+        // For example, if there's a button with the ID 1234, you can simulate a click:
+        // unsafe {
+        //     SendMessageA(window_handle, BM_CLICK, 1234 as usize, ptr::null_mut());
+        // }
+    } else {
+        println!("EverQuest window not found");
+    }
 }
 
 // This works but ONLY for Notepad
