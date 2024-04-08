726dbed17757caca4a163da56a2114fd59783320
RdotSilva <RyandotSilva@gmail.com>
2024-04-08 18:36:54 +0100

    Add basic nav bar list

--- src/components/SideNavigationBar/SideNavigationBar.tsx
+++ src/components/SideNavigationBar/SideNavigationBar.tsx
@@ -1,12 +1,16 @@
-import { List } from "@mui/material";
-import LocDetails from "../LocDetails/LocDetails";
+import { List, ListItem, ListItemText } from "@mui/material";
+import useFilterItems from "../../hooks/useFilterItems";
 
 const SideNavigationBar = ({ items }) => {
+  const { uniqueZones } = useFilterItems(items);
+
   return (
     <div>
       <List>
-        {items.map((item, index) => (
-          <LocDetails key={index} item={item} />
+        {uniqueZones.map((zone, index) => (
+          <ListItem key={index} onClick={() => console.log("TODO")}>
+            <ListItemText primary={zone} />
+          </ListItem>
         ))}
       </List>
     </div>
