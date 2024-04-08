fe9ff51e70a24f93ad108454e7205ac797a3e798
RdotSilva <RyandotSilva@gmail.com>
2024-04-08 18:45:07 +0100

    Add handleZoneChange for buttons

--- src/components/SideNavigationBar/SideNavigationBar.tsx
+++ src/components/SideNavigationBar/SideNavigationBar.tsx
@@ -2,13 +2,13 @@ import { List, ListItem, ListItemText } from "@mui/material";
 import useFilterItems from "../../hooks/useFilterItems";
 
 const SideNavigationBar = ({ items }) => {
-  const { uniqueZones } = useFilterItems(items);
+  const { uniqueZones, handleZoneChange } = useFilterItems(items);
 
   return (
     <div>
       <List>
         {uniqueZones.map((zone, index) => (
-          <ListItem key={index} onClick={() => console.log("TODO")}>
+          <ListItem button key={index} onClick={() => handleZoneChange(zone)}>
             <ListItemText primary={zone} />
           </ListItem>
         ))}
